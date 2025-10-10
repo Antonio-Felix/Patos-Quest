ec487a16d553ef4e4667c8b1e269694eebd762aa
Antonio <antonio980felixmedeiros@gmail.com>
2025-10-10 13:46:55 -0300

    update clean code + collision

--- Projeto/patos-quest/cenas/game_level.gd
+++ Projeto/patos-quest/cenas/game_level.gd
@@ -3,21 +3,8 @@ class_name GameLevel
 
 const _DIALOG_SCREEN: PackedScene = preload("res://dialog_screen.tscn")
 
-var _dialog_data: Dictionary = {
-	0: {
-		"faceset": "res://personagens/3x4_Dona_Lucia.png",
-		"dialog": "Olá Gui, vá conhecer a praça hoje",
-		"title": "Dona Lúcia"
-	},
-	1: {
-		"faceset": "res://personagens/3x4_Dona_Lucia.png",
-		"dialog": "Tchau!Te amo!!!",
-		"title": "Dona Lúcia"
-	}
-}
-
 @export_category("Objects")
-@export var _hud: CanvasLayer = null
+# @export var _hud: CanvasLayer = null
 var dialogue1 = preload("res://conversa.dialogue")
 var player_in_area := false
 var id: int = 0
@@ -28,14 +15,6 @@ func _ready() -> void:
 	DialogueManager.show_example_dialogue_balloon(dialogue1, "bom_dia")
 
 func _process(_delta: float) -> void: 
-	if Input.is_action_just_pressed("ui_select"):
-		for child in _hud.get_children():
-			if child is DialogScreen:
-				return
-		var _new_dialog = _DIALOG_SCREEN.instantiate() as DialogScreen
-		_new_dialog.data = _dialog_data
-		_hud.add_child(_new_dialog)
-		
 	if player_in_area and Input.is_action_just_pressed("E"):
 		if id == 1: DialogueManager.show_example_dialogue_balloon(dialogue1, "DG") 
 		elif id == 2: DialogueManager.show_example_dialogue_balloon(dialogue1, "claraAurora") 
