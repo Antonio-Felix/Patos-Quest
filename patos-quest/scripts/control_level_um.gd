6f3f29239e80a74b515872be926426346a89fa7c
Antonio <antonio980felixmedeiros@gmail.com>
2025-11-27 16:42:19 -0300

    delete folder

--- patos-quest/scripts/control_level_um.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-@onready var npc_counter: Label = $container/npc_container/npc_counter as Label
-
-func _ready() -> void:
-	npc_counter.text = str("%02d" % Globals.npc_dialog)
-
-func _process(_delta: float) -> void:
-	npc_counter.text = str("%02d" % Globals.npc_dialog)
--- patos-quest/scripts/splash_screen.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:	
-	if anim_name == "fade_in":
-		$AnimationPlayer.play("fade_out")
-	
-	if anim_name == "fade_out":
-		get_tree().change_scene_to_file("res://cenas/menu_principal.tscn")
